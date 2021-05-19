b4a48cbff4fb6ae21fc8d3d7727b2dbb68b1b451
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-05-19 18:39:29 -0300

    change: local files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: "app.component.html" 
+  templateUrl: "app.component.html",
    
 })
 export class AppComponent {
-  title = 'frontend';
-  nome = "Vitor";
+  
 }
